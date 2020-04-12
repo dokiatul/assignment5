a88a840286bd810207e22ef3b44dd41c99d85f41
dokiatul <dokiatul@gmail.com>
2020-04-11 19:10:19 -0700

    Removed eslint errors

--- ui/src/ProductEdit.jsx
+++ ui/src/ProductEdit.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/label-has-for */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+
 import React from 'react';
 import { Link } from 'react-router-dom';
 
--- ui/src/ProductTable.jsx
+++ ui/src/ProductTable.jsx
@@ -3,26 +3,24 @@ import { Link, withRouter } from 'react-router-dom';
 
 const ProductRow = withRouter(({
   product, deleteProduct, index,
-}) => {
-  return (
-    <tr>
-      <td>{product.name}</td>
-      <td>
-        $
-        {product.price}
-      </td>
-      <td>{product.category}</td>
-      <td><Link to={`/view/${product.id}`} target="_blank" rel="noopener noreferrer">Veiw</Link></td>
-      <td>
-        <Link to={`/edit/${product.id}`}>Edit</Link>
-        {' | '}
-        <button type="button" onClick={() => { deleteProduct(index); }}>
-          Delete
-        </button>
-      </td>
-    </tr>
-  );
-});
+}) => (
+  <tr>
+    <td>{product.name}</td>
+    <td>
+      $
+      {product.price}
+    </td>
+    <td>{product.category}</td>
+    <td><Link to={`/view/${product.id}`} target="_blank" rel="noopener noreferrer">Veiw</Link></td>
+    <td>
+      <Link to={`/edit/${product.id}`}>Edit</Link>
+      {' | '}
+      <button type="button" onClick={() => { deleteProduct(index); }}>
+        Delete
+      </button>
+    </td>
+  </tr>
+));
 
 export default function ProductTable({ products, deleteProduct }) {
   const productRows = products.map((product, index) => (
